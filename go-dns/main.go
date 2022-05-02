e5fcb68ff0bd395ea25abc6f4218f198305ded63
Aditya Vikram <65796572+adi-dev-vikram@users.noreply.github.com>
2022-05-02 23:11:16 +0530

    Add files via upload

---
+++ go-dns/main.go
@@ -0,0 +1,31 @@
+package main
+   import (
+      "fmt"
+      "net"
+	  "os" )
+
+func main(){
+	arguments := os.Args
+if len(arguments) == 1 {
+fmt.Println("Please provide an IP address!") os.Exit(100)
+}
+IP := arguments[1] addr := net.ParseIP(IP) if addr == nil {
+fmt.Println("Not a valid IP address!") os.Exit(100)
+
+hosts, err := net.LookupAddr(IP) if err != nil {
+	fmt.Println(err) os.Exit(100)
+	}
+	for _, hostname := range hosts { fmt.Println(hostname)
+	}
+
+	hostname := arguments[1]
+	IPs, err := net.LookupHost(hostname)
+	if err != nil { 
+		fmt.Println(err)
+		os.Exit(100)
+		}
+		for _, IP := range IPs { 
+			fmt.Println(IP)
+		} }
+
+}
