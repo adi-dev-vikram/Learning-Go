8aedb3489f12a5648400983966e3a991a5a6f90a
Aditya Vikram <65796572+adi-dev-vikram@users.noreply.github.com>
2022-06-06 22:09:44 +0530

    Add files via upload

--- go-dns/main.go
+++ go-dns/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"os"
 	"strings"
-
-	"golang.org/x/net/publicsuffix"
 )
 
-func checkForValidTLD(str string) bool {
+/*func checkForValidTLD(str string) bool {
 	etld, im := publicsuffix.PublicSuffix(str)
 	var validtld = false
 	if im { // ICANN managed
@@ -19,52 +17,101 @@ func checkForValidTLD(str string) bool {
 	}
 	return validtld
 }
+*/
+func checkDomain(domain string) {
+	var hasMX, hasSPF, hasdmarc bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMX = true
+
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+
+	}
+
+}
 
 func main() {
-	arguments := os.Args
+	res := strings.SplitN("abc@goole.com", "@", 2)
 
-	if len(arguments) == 1 {
-		fmt.Println("Please provide an IP address!")
-		os.Exit(100)
-	}
+	fmt.Printf("\nSlice 1: %s", res[1])
 
-	IP := arguments[1]
-	addr := net.ParseIP(IP)
-	if addr == nil {
-		fmt.Println("Not a valid IP address!")
-		os.Exit(100)
-	}
+	/*
+		scanner := bufio.NewScanner(os.Stdin)
 
-	hosts, err := net.LookupAddr(IP)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(100)
-	}
-	for _, hostname := range hosts {
-		fmt.Println(hostname)
-	}
+		fmt.Printf("domain,hasMX,hasSPF,hasDMARC,dmarcRecord\n")
 
-	hostname := arguments[2]
-	IPs, err := net.LookupHost(hostname)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(100)
-	}
-	for _, IP := range IPs {
-		fmt.Println(IP)
-	}
+		for scanner.Scan() {
+			checkDomain(scanner.Text())
+		}
 
-	//DNS-related problems and finding out the status of a domain
-	//using NS lookup
+		err := scanner.Err()
+		if err != nil {
+			log.Fatal("issue in scanning")
+		}
+		arguments := os.Args
 
-	domain := arguments[3]
-	NSs, err := net.LookupNS(domain)
+		if len(arguments) == 1 {
+			fmt.Println("Please provide an IP address!")
+			os.Exit(100)
+		}
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(100)
-	}
-	for _, NS := range NSs {
-		fmt.Println(NS.Host)
-	}
+		IP := arguments[1]
+		addr := net.ParseIP(IP)
+		if addr == nil {
+			fmt.Println("Not a valid IP address!")
+			os.Exit(100)
+		}
+
+		hosts, err := net.LookupAddr(IP)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(100)
+		}
+		for _, hostname := range hosts {
+			fmt.Println(hostname)
+		}
+
+		hostname := arguments[2]
+		IPs, err := net.LookupHost(hostname)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(100)
+		}
+		for _, IP := range IPs {
+			fmt.Println(IP)
+		}
+
+		//DNS-related problems and finding out the status of a domain
+		//using NS lookup
+
+		domain := arguments[3]
+		NSs, err := net.LookupNS(domain)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(100)
+		}
+		for _, NS := range NSs {
+			fmt.Println(NS.Host)
+		}
+	*/
 }
